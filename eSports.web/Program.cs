2dd930a22e36a9f1870e9bc567f90c0d844e2d03
hrshuv0 <h.r.shuvo9@gmail.com>
2022-08-07 12:45:37 +0600

    added area +configuration

--- eSports.web/Program.cs
+++ eSports.web/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Player}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
