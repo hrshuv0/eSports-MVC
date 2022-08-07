3519c35b3f7eb30ffb7e2a04d60b24d518335df1
hrshuv0 <h.r.shuvo9@gmail.com>
2022-08-07 11:39:35 +0600

    added create option - game

--- eSports.entities/Models/Game.cs
+++ eSports.entities/Models/Game.cs
@@ -1,8 +1,12 @@
-﻿namespace eSports.entities.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eSports.entities.Models;
 
 public class Game
 {
     public int Id { get; set; }
+    
+    [Required]
     public string? Name { get; set; }
     
 }
---
+++ eSports.web/Controllers/GamesController.cs
@@ -0,0 +1,40 @@
+﻿using eSports.dal.Data;
+using eSports.entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSports.web.Controllers;
+
+public class GamesController : Controller
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public GamesController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Game model)
+    {
+        if (!ModelState.IsValid) return View(model);
+
+        _dbContext.Games!.Add(model);
+        _dbContext.SaveChanges();
+        
+        return RedirectToAction(nameof(Index));
+    }
+    
+    
+}
