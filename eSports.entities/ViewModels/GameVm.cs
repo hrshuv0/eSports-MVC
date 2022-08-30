a642da0c1f4de3857227cc3daa3a44f59de45e50
hrshuv0 <h.r.shuvo9@gmail.com>
2022-08-30 18:05:01 +0600

    added game - create

---
+++ eSports.entities/ViewModels/GameVm.cs
@@ -0,0 +1,13 @@
+﻿using eSports.entities.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace eSports.entities.ViewModels;
+
+public class GameVm
+{
+    public Game Game { get; set; }
+
+    [ValidateNever]
+    public IEnumerable<SelectListItem>? CategoryList { get; set; }
+}
--- eSports.web/Areas/Admin/Controllers/GamesController.cs
+++ eSports.web/Areas/Admin/Controllers/GamesController.cs
@@ -1,6 +1,8 @@
 ﻿using eSports.dal.Repository.IRepository;
 using eSports.entities.Models;
+using eSports.entities.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace eSports.web.Areas.Admin.Controllers;
 
@@ -23,16 +25,26 @@ public class GamesController : Controller
 
     public IActionResult Create()
     {
-        return View();
+        var gameVm = new GameVm()
+        {
+            Game = new Game(),
+            CategoryList = _unitOfWork.Category.GetAll()!.Select(c => new SelectListItem()
+            {
+                Text = c.Name,
+                Value = c.Id.ToString()
+            })
+        };
+        
+        return View(gameVm);
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Create(Game model)
+    public IActionResult Create(GameVm model)
     {
         if (!ModelState.IsValid) return View(model);
-
-        _unitOfWork.Game.Add(model);
+        
+        _unitOfWork.Game.Add(model.Game);
         _unitOfWork.Save();
         
         return RedirectToAction(nameof(Index));
