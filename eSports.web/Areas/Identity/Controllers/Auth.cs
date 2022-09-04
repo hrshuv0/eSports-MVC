977b7641ec792edb3c1c506c784831ccbeb5881b
hrshuv0 <h.r.shuvo9@gmail.com>
2022-09-04 17:49:18 +0600

    added register

---
+++ eSports.web/Areas/Identity/Controllers/Auth.cs
@@ -0,0 +1,61 @@
+﻿using eSports.entities.Models;
+using eSports.utility.StaticData;
+using eSports.web.Areas.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSports.web.Areas.Identity.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    // GET
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    // POST
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterVm model)
+    {
+        if (!ModelState.IsValid) return View(model);
+
+        var user = await _userManager.FindByNameAsync(model.UserName);
+
+        if (user is not null)
+        {
+            ModelState.AddModelError(string.Empty, "user already exists");
+            return View(model);
+        }
+
+        var newUser = new ApplicationUser()
+        {
+            UserName = model.UserName
+        };
+        var response = await _userManager.CreateAsync(newUser, model.Password);
+
+        if (!response.Succeeded)
+        {
+            return View(model);
+        }
+
+        await _userManager.AddToRoleAsync(newUser, UserRoles.Player);
+
+        var result = await _signInManager.PasswordSignInAsync(newUser, model.Password, false, false);
+        if (result.Succeeded)
+        {
+            return RedirectToAction("Index", "Home", new { area = "Player" });
+        }
+
+        return View(model);
+    }
+}
---
+++ eSports.web/Areas/Identity/Models/RegisterVm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eSports.web.Areas.Identity.Models;
+
+public class RegisterVm
+{
+    [Required(ErrorMessage = "user name is required")]
+    [Display(Name = "User Name")]
+    public string? UserName { get; set; }
+
+    [Required(ErrorMessage = "password is required")]
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+
+    [Display(Name = "Confirm Password")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "password didn't password")]
+    public string? ConfirmPassword { get; set; }
+}
