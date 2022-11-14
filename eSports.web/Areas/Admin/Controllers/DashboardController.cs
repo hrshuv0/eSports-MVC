85255cac38f30c928712722353b8429d6ee14acc
hrshuv0 <h.r.shuvo9@gmail.com>
2022-11-14 16:47:52 +0600

    added theme for admin - sleek

---
+++ eSports.web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eSports.web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class DashboardController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Analytics()
+    {
+        return View();
+    }
+}
