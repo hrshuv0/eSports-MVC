4d4d9ef65ce268a5ee0499ff23b89419d77f742c
hrshuv0 <h.r.shuvo9@gmail.com>
2022-11-19 17:58:11 +0600

    added game details - player

---
+++ eSports.web/Areas/Player/Controllers/TournamentController.cs
@@ -0,0 +1,23 @@
+﻿using eSports.dal.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSports.web.Areas.Player.Controllers;
+
+[Area("Player")]
+public class TournamentController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+    
+    public TournamentController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    // GET
+    public IActionResult Index()
+    {
+        var tournamentList = _unitOfWork.Tournament.GetAll(includeProperties:"Game,TournamentCategory,Prize");
+        
+        return View(tournamentList);
+    }
+}
